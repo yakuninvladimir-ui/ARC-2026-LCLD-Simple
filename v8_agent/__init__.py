bdc18aae7ef2a07de7ed09c673cd58930c087023
Vladimir Yakunin <yakunin.vladimir@gmail.com>
2026-08-11 09:38:18 +0400

    delete v8

--- v8_agent/__init__.py
+++
@@ -1,5 +0,0 @@
-from .config import V8Config, config_from_mapping, default_config_dict
-from .session import GameSession, LevelRunLimitReached
-
-__version__ = "8.3.0"
-__all__ = ["V8Config", "config_from_mapping", "default_config_dict", "GameSession", "LevelRunLimitReached", "__version__"]
--- v8_agent/coordinate_research.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from .arga_lite import generate_coordinate_targets
-
-__all__ = ["generate_coordinate_targets"]
